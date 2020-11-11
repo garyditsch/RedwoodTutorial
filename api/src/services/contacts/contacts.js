1222c1205fa7cd20e6077efebff55305d1213794
Gary Ditsch <garyditsch@gmail.com>
2020-11-10 20:53:28 -0500

    tutorial up through saving data

---
+++ api/src/services/contacts/contacts.js
@@ -0,0 +1,23 @@
+import { db } from 'src/lib/db'
+
+import { UserInputError } from '@redwoodjs/api'
+
+
+const validate = (input) => {
+  if (input.email && !input.email.match(/[^@]+@[^.]+\..+/)) {
+    throw new UserInputError("Can not create new contact", {
+      messages: {
+        email: ['is not formatted like an email address'],
+      },
+    })
+  }
+}
+
+export const contacts = () => {
+  return db.contact.findMany()
+}
+
+export const createContact = ({ input }) => {
+  validate(input)
+  return db.contact.create({ data: input })
+}
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostCell from 'src/components/BlogPostsCell/BlogPostsCell'
+
+const HomePage = () => {
+  return <BlogLayout>
+    <BlogPostCell />
+  </BlogLayout>
+}
+
+export default HomePage
